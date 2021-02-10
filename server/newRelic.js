0957383456fede96ffb1a06d80a91dee62117655
Joel Rubin <68345201+joelsrubin@users.noreply.github.com>
2021-02-10 16:58:40 -0600

    Update newRelic.js

--- server/newRelic.js
+++ server/newRelic.js
@@ -13,7 +13,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key: '426afc582641477c7fd6a7faba44bbecde70NRAL',
+  license_key: '',
   /**
    * This setting controls distributed tracing.
    * Distributed tracing lets you see the path that a request takes through your
